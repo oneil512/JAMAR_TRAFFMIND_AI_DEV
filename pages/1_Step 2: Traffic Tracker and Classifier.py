7a09669824335f602c77d725498b4a5836780b14
Edward Ayoub <114652033+edwardayoub@users.noreply.github.com>
2024-05-09 17:01:05 -0500

    Update and rename 1_Step 2: Traffic Tracker.py to 1_Step 2: Traffic Tracker and Classifier.py

--- pages/1_Step 2: Traffic Tracker and Classifier.py
+++ pages/1_Step 2: Traffic Tracker and Classifier.py
@@ -10,7 +10,10 @@ st.set_page_config(page_title="Traffic Tracker - Processed Videos", layout="wide
 st.header("TraffMind AI Traffic Tracker")
 
 st.markdown("""
-Experience our Traffic Tracker's capabilities firsthand. This feature automatically identifies and tracks vehicles with bounding boxes, enhancing traffic video analysis. Follow the steps below to view and download your processed videos:
+Experience our Traffic Tracker's capabilities firsthand. This feature automatically identifies and tracks vehicles with bounding boxes, enhancing traffic video analysis. This powerful tool can categorize vehicles into:
+- **6 bins**: For basic classification needs, such as distinguishing between motorcycles, cars, vans/light trucks, buses, single unit trucks, and combination trucks.
+
+Follow the steps below to view and download your processed videos:
 """)
 st.markdown("""
 **1. Refresh Data**: Click the button below to refresh the list of processed videos.
@@ -26,7 +29,10 @@ if 'first_load' not in st.session_state:
 
 # Auto-refresh on the initial load or when the refresh button is pressed
 if 'first_load' not in st.session_state or refresh:
-    data_df = get_s3_status()
-    # Display data
-    show_table_with_links(data_df)
-    st.session_state['first_load'] = False
+    try:
+        data_df = get_s3_status()
+        show_table_with_links(data_df)
+        st.session_state['first_load'] = False
+    except Exception as e:
+        st.error(f"No jobs have been submitted yet. Please submit a job to view processed videos.")
+        st.stop()
