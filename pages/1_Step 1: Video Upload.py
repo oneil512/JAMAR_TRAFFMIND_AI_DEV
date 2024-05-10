fc9fc79b323d2554def827cf182c4f491ae85ec2
Clay O'Neil <oneil512@umn.edu>
2024-05-10 12:34:58 -0500

    bugfix

--- pages/1_Step 1: Video Upload.py
+++ pages/1_Step 1: Video Upload.py
@@ -73,7 +73,38 @@ st.markdown("""
 """)
 
 # File uploader for video selection
-uploaded_video = st.file_uploader("Upload your video", type=['mp4', 'h264'], accept_multiple_files=False, on_change=upload)
+uploaded_video = st.file_uploader("Upload your video", type=['mp4', 'h264'], accept_multiple_files=False)
+
+if uploaded_video is not None:
+    print(f"Uploaded video: {uploaded_video}")
+    st.sidebar.success("Your submission is received!")
+    print(uploaded_video.name)
+
+    # Read keys in from environment variables
+    access_key = os.getenv("AWS_ACCESS_KEY_ID")
+    secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+
+    s3_client = boto3.client(
+        "s3",
+        region_name='us-east-2',
+        aws_access_key_id=access_key,
+        aws_secret_access_key=secret_key
+    )
+    url = generate_presigned_url(
+        s3_client,
+        "put_object",
+        {"Bucket": 'traffmind-client-unprocessed-jamar-dev', "Key": uploaded_video.name},
+        1000
+    )
+    print(url)
+    
+    response = requests.put(url, data=uploaded_video.getvalue())
+
+    if response.status_code == 200:
+        run(uploaded_video.name)
+
+else:
+    st.sidebar.error("Please upload a video and provide a name for your submission.")
 
 # Step 2: Submit
 st.markdown("""
