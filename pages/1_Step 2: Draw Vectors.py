0e0e8a94711908f9141061a2418277eec84a3961
Edward Ayoub <114652033+edwardayoub@users.noreply.github.com>
2024-06-20 22:53:01 -0500

    Update 1_Step 2: Draw Vectors.py

--- pages/1_Step 2: Draw Vectors.py
+++ pages/1_Step 2: Draw Vectors.py
@@ -1,52 +1,37 @@
 import streamlit as st
 import pandas as pd
 from PIL import Image
-import requests
 from io import BytesIO
-import subprocess
-import sys
-import time
+from streamlit_drawable_canvas import st_canvas
+import requests
 
-try:
-    from streamlit_drawable_canvas import st_canvas
+def get_background_image_url(url):
+    base_url_path = st._config.get_option("server.baseUrlPath").strip("/")
+    if base_url_path:
+        base_url_path = "/" + base_url_path
+    return base_url_path + url
 
-# This block executes only on the first run when your package isn't installed
-except ModuleNotFoundError as e:
-    sleep_time = 30
-    dependency_warning = st.warning(
-        f"Installing dependencies, this takes {sleep_time} seconds."
-    )
-    subprocess.Popen(
-        [
-            f"{sys.executable} -m pip install git+https://github.com/edwardayoub/streamlit-drawable-canvas.git",
-        ],
-        shell=True,
-    )
-
-    # wait for subprocess to install package before running your actual code below
-    time.sleep(sleep_time)
-    # remove the installing dependency warning
-    dependency_warning.empty()
-
-
-# from streamlit_drawable_canvas import st_canvas
-
-# Load background image from URL
+# URL of the background image
 background_image_url = "https://www.crowsonlaw.com/wp-content/webp-express/webp-images/uploads/2023/11/right-of-way-rules.jpg.webp"
-response = requests.get(background_image_url)
+
+# Modify the URL if necessary
+modified_image_url = get_background_image_url(background_image_url)
+
+# Load background image from the modified URL
+response = requests.get(modified_image_url)
 bg_image = Image.open(BytesIO(response.content))
 
-# Create a canvas component with fixed settings
+# Create a canvas component
 canvas_result = st_canvas(
-    fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
-    stroke_width=3,  # Fixed stroke width
-    stroke_color="rgba(0, 0, 255, 1)",  # Fixed stroke color
-    background_color="#eee",  # Fixed background color
+    fill_color="rgba(255, 165, 0, 0.3)",
+    stroke_width=3,
+    stroke_color="rgba(0, 0, 255, 1)",
+    background_color="#eee",
     background_image=bg_image,
-    update_streamlit=True,  # Always update in real time
+    update_streamlit=True,
     height=400,
     width=600,
-    drawing_mode="line",  # Always in line drawing mode
+    drawing_mode="line",
     display_toolbar=False,
     key="full_app",
 )
