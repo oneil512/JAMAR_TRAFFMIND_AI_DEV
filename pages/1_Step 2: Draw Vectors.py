d7f782388ce70a2c7cd17626c3592a4288f3737c
Edward Ayoub <114652033+edwardayoub@users.noreply.github.com>
2024-06-20 21:25:56 -0500

    Update 1_Step 2: Draw Vectors.py

--- pages/1_Step 2: Draw Vectors.py
+++ pages/1_Step 2: Draw Vectors.py
@@ -1,32 +1,10 @@
+import streamlit as st
 import pandas as pd
 from PIL import Image
 import requests
 from io import BytesIO
-import subprocess
-import sys
-import time
-import streamlit as st
 
-try:
-    from streamlit_drawable_canvas import st_canvas
-
-# This block executes only on the first run when your package isn't installed
-except ModuleNotFoundError as e:
-    sleep_time = 30
-    dependency_warning = st.warning(
-        f"Installing dependencies, this takes {sleep_time} seconds."
-    )
-    subprocess.Popen(
-        [
-            f"{sys.executable} -m pip install git+https://github.com/edwardayoub/streamlit-drawable-canvas.git",
-        ],
-        shell=True,
-    )
-
-    # wait for subprocess to install package before running your actual code below
-    time.sleep(sleep_time)
-    # remove the installing dependency warning
-    dependency_warning.empty()
+# from streamlit_drawable_canvas import st_canvas
 
 # Load background image from URL
 background_image_url = "https://www.crowsonlaw.com/wp-content/webp-express/webp-images/uploads/2023/11/right-of-way-rules.jpg.webp"
