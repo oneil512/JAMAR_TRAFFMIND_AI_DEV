abe9bdf1b9b466f6557b09436f93e7cb94203d2d
Edward Ayoub <114652033+edwardayoub@users.noreply.github.com>
2024-06-21 00:27:34 -0500

    Update __init__.py

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,4 +1,4 @@
 from .sagemaker_processing import run
-from .aws import download_file, list_files, generate_presigned_url, send_discord_notification
+from .aws import download_file, list_files, generate_presigned_url, send_discord_notification, list_files_paginated
 
-__all__ = ["run", "download_file", "list_files", "generate_presigned_url", "send_discord_notification"]
+__all__ = ["run", "download_file", "list_files", "generate_presigned_url", "send_discord_notification", "list_files_paginated"]
