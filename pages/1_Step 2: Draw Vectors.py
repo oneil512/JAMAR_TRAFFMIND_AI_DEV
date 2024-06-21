3efc297ae4ebf0f747b6c11ac07ad1998b026b03
Edward Ayoub <114652033+edwardayoub@users.noreply.github.com>
2024-06-20 23:22:29 -0500

    Update 1_Step 2: Draw Vectors.py

--- pages/1_Step 2: Draw Vectors.py
+++ pages/1_Step 2: Draw Vectors.py
@@ -1,41 +1,26 @@
 import streamlit as st
 import pandas as pd
 from PIL import Image
+import requests
 from io import BytesIO
 from streamlit_drawable_canvas import st_canvas
-import requests
 
-def get_background_image_url(url):
-    base_url_path = st._config.get_option("server.baseUrlPath")
-    if base_url_path:
-        base_url_path = "/" + base_url_path
-    return base_url_path + url
-
-# Print the base URL path
-base_url_path = st._config.get_option("server.baseUrlPath")
-st.write(f"Base URL Path: {base_url_path}")
-
-# URL of the background image
+# Load background image from URL
 background_image_url = "https://www.crowsonlaw.com/wp-content/webp-express/webp-images/uploads/2023/11/right-of-way-rules.jpg.webp"
-
-# Modify the URL if necessary
-modified_image_url = get_background_image_url(background_image_url)
-
-# Load background image from the modified URL
-response = requests.get(modified_image_url)
+response = requests.get(background_image_url)
 bg_image = Image.open(BytesIO(response.content))
 
-# Create a canvas component
+# Create a canvas component with fixed settings
 canvas_result = st_canvas(
-    fill_color="rgba(255, 165, 0, 0.3)",
-    stroke_width=3,
-    stroke_color="rgba(0, 0, 255, 1)",
-    background_color="#eee",
+    fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+    stroke_width=3,  # Fixed stroke width
+    stroke_color="rgba(0, 0, 255, 1)",  # Fixed stroke color
+    background_color="#eee",  # Fixed background color
     background_image=bg_image,
-    update_streamlit=True,
+    update_streamlit=True,  # Always update in real time
     height=400,
     width=600,
-    drawing_mode="line",
+    drawing_mode="line",  # Always in line drawing mode
     display_toolbar=False,
     key="full_app",
 )
