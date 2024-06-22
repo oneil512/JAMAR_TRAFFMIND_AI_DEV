93fad9f105ef0ffb52923ba65258a36324b9c7f9
Edward Ayoub <114652033+edwardayoub@users.noreply.github.com>
2024-06-22 01:31:11 -0500

    Update 1_Step 2: Job Submission.py

--- pages/1_Step 2: Job Submission.py
+++ pages/1_Step 2: Job Submission.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 from io import BytesIO
 from streamlit_drawable_canvas import st_canvas
 from lib.aws import list_files_paginated, extract_first_frame, convert_lines_to_vectors, write_vectors_to_s3
@@ -75,69 +75,71 @@ if 'bg_image' in st.session_state:
         key="canvas",
     )
 
-    st.markdown("""
+st.markdown("""
 2. **Label Vectors**:
     - Click the 'Label Vectors' button below to proceed to labeling the directions for each vector.
-    """)
+""")
 
-    if st.button("Label Vectors"):
-        if canvas_result.json_data is not None:
-            objects = pd.json_normalize(canvas_result.json_data["objects"])
-            for col in objects.select_dtypes(include=["object"]).columns:
-                objects[col] = objects[col].astype("str")
+if st.button("Label Vectors"):
+    if canvas_result.json_data is not None:
+        objects = pd.json_normalize(canvas_result.json_data["objects"])
+        for col in objects.select_dtypes(include=["object"]).columns:
+            objects[col] = objects[col].astype("str")
 
-            if not objects.empty:
-                vectors = []
-                for _, row in objects.iterrows():
-                    if row["type"] == "line":
-                        left, top, width, height = float(row["left"]), float(row["top"]), float(row["width"]), float(row["height"])
-                        center_x = left + width / 2
-                        center_y = top + height / 2
-                        x1 = center_x + float(row["x1"])
-                        y1 = center_y + float(row["y1"])
-                        x2 = center_x + float(row["x2"])
-                        y2 = center_y + float(row["y2"])
-                        vectors.append((x1, y1, x2, y2))
+        if not objects.empty:
+            vectors = []
+            for _, row in objects.iterrows():
+                if row["type"] == "line":
+                    left, top, width, height = float(row["left"]), float(row["top"]), float(row["width"]), float(row["height"])
+                    center_x = left + width / 2
+                    center_y = top + height / 2
+                    x1 = center_x + float(row["x1"])
+                    y1 = center_y + float(row["y1"])
+                    x2 = center_x + float(row["x2"])
+                    y2 = center_y + float(row["y2"])
+                    vectors.append((x1, y1, x2, y2))
 
-                st.session_state['vectors'] = vectors
-                st.session_state['names_to_vectors'][bg_video_name] = vectors
-            else:
-                st.error("No vectors found. Please draw vectors first.")
+            st.session_state['vectors'] = vectors
+            st.session_state['names_to_vectors'][bg_video_name] = vectors
         else:
             st.error("No vectors found. Please draw vectors first.")
+    else:
+        st.error("No vectors found. Please draw vectors first.")
 
-    st.markdown("""
+st.markdown("""
 3. **Review Labeling**:
     - Review the vectors and their labels in the image preview below.
-    """)
+""")
 
-    col1, col2 = st.columns([3, 1])
-    with col1:
-        if 'vectors' in st.session_state and st.session_state['vectors']:
-            img = Image.open(BytesIO(bg_image_bytes))
-            draw = ImageDraw.Draw(img)
+col1, col2 = st.columns([3, 1])
+with col1:
+    if 'vectors' in st.session_state and st.session_state['vectors']:
+        img = Image.open(BytesIO(bg_image_bytes))
+        draw = ImageDraw.Draw(img)
+        font_path = os.path.join(cv2.__path__[0], 'qt', 'fonts', 'DejaVuSans.ttf')
+        font = ImageFont.truetype(font_path, size=50)  # Increase font size
 
-            for i, (x1, y1, x2, y2) in enumerate(st.session_state['vectors']):
-                draw.line((x1, y1, x2, y2), fill=(255, 0, 0), width=3)
-                text_x = (x1 + x2) / 2
-                text_y = (y1 + y2) / 2 - 10
-                draw.text((text_x, text_y), f"{i + 1}", fill=(0, 0, 0))
+        for i, (x1, y1, x2, y2) in enumerate(st.session_state['vectors']):
+            draw.line((x1, y1, x2, y2), fill=(255, 0, 0), width=3)
+            text_x = (x1 + x2) / 2
+            text_y = (y1 + y2) / 2 - 25
+            draw.text((text_x, text_y), f"{i + 1}", fill=(255, 0, 0), font=font)  # Draw text in red
 
-            st.image(img, caption="Review your vectors and labels", use_column_width=True)
+        st.image(img, caption="Review your vectors and labels", use_column_width=True)
 
-    with col2:
-        if 'vectors' in st.session_state:
-            for i, (x1, y1, x2, y2) in enumerate(st.session_state['vectors']):
-                st.write(f":blue[Vector {i + 1}]")
-                directions_list = ["N", "E", "S", "W"]
-                option = st.selectbox(f"Vector {i + 1} Direction", directions_list, key=f"direction_{i}")
-                if option:
-                    handle_click(option, i)
+with col2:
+    if 'vectors' in st.session_state:
+        for i, (x1, y1, x2, y2) in enumerate(st.session_state['vectors']):
+            st.write(f":blue[Vector {i + 1}]")
+            directions_list = ["N", "E", "S", "W"]
+            option = st.selectbox(f"Vector {i + 1} Direction", directions_list, key=f"direction_{i}")
+            if option:
+                handle_click(option, i)
 
 st.markdown("""
 4. **Submit Job**:
     - Once all vectors are drawn and directions are specified, click the 'Submit Job' button to submit your video for processing.
-    """)
+""")
 
 # Add a button to submit the job
 if st.button("Submit Job"):
