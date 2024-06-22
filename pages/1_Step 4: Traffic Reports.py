15b1cdc097d50d9a3119613bb89bb29a2390aecd
Edward Ayoub <114652033+edwardayoub@users.noreply.github.com>
2024-06-22 10:09:49 -0500

    Update and rename 1_Step 4: Traffic Counter.py to 1_Step 4: Traffic Reports.py

--- pages/1_Step 4: Traffic Reports.py
+++ pages/1_Step 4: Traffic Reports.py
@@ -3,50 +3,46 @@ from lib.aws import list_files_paginated, download_file
 import pandas as pd
 
 # Set page configuration
-st.set_page_config(page_title="TraffMind AI Traffic Counter", layout="wide")
+st.set_page_config(page_title="TraffMind AI Job Reports", layout="wide")
 
-st.header("TraffMind AI Traffic Counter")
+st.header("TraffMind AI Job Reports")
 
 # Manage initial load and refresh with session state
 if 'first_load' not in st.session_state or 'names' not in st.session_state or 'name_to_key' not in st.session_state:
     name_to_key = {}
     names = list_files_paginated("jamar","outputs/", file_type='txt')
-    # get just file names
+    # Get just file names
     name_to_key = {name.split('/')[-1]: name for name in names}
 
-    # filter out keys that have 'crosswalk' in them
+    # Filter out keys that have 'crosswalk' in them
     names = [name for name in names if 'crosswalk' not in name]
-
 
     names = [name.split('/')[-1] for name in names]
     st.session_state['first_load'] = True
     st.session_state['names'] = names
     st.session_state['name_to_key'] = name_to_key
 
-refresh = st.button('Refresh Counts', key='refresh')
+refresh = st.button('Refresh Reports', key='refresh')
 
-# Dropdown for selecting counts file
-count_file_name = st.selectbox("Select a count file", st.session_state.get('names', []))
+# Dropdown for selecting report file
+report_file_name = st.selectbox("Select a report file", st.session_state.get('names', []))
 
-if count_file_name:
-    # download the file from s3
+if report_file_name:
+    # Download the file from S3
     if not st.session_state.get('name_to_key', False):
         st.rerun()
 
-    key = st.session_state['name_to_key'][count_file_name]
-    download_file("jamar", key, count_file_name)
+    key = st.session_state['name_to_key'][report_file_name]
+    download_file("jamar", key, report_file_name)
 
     minute_increment = 15
 
-
-    count_file_path = count_file_name
+    report_file_path = report_file_name
     try:
-        with open(count_file_path, 'r') as f:
-
+        with open(report_file_path, 'r') as f:
             movement_dict_list = eval(f.readlines()[0])
-            # remove empty dicts from list
+            # Remove empty dicts from list
             movement_dict_list = [x for x in movement_dict_list if x]
-            # remove index column
 
             rows = []
 
@@ -61,18 +57,15 @@ if count_file_name:
                     rows.append(row)
             final_df = pd.DataFrame(rows)
 
-            # convert na to 0
+            # Convert NA to 0
             final_df = final_df.fillna(0)
-            # convert all columns except From and To to int
+            # Convert all columns except From and To to int
             for col in final_df.columns:
                 if col not in ['From', 'To']:
                     final_df[col] = final_df[col].astype(int)
 
             n_rows = final_df.shape[0]
 
-            st.dataframe(final_df.style.hide(axis="index"), hide_index=True, height = int(35.2*(n_rows+1)), width=1000)
+            st.dataframe(final_df.style.hide(axis="index"), hide_index=True, height=int(35.2 * (n_rows + 1)), width=1000)
     except Exception as e:
-        st.error(f"Error loading counts file: {e}")
-
-
-
+        st.error(f"Error loading report file: {e}")
